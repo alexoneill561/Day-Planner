c55ceac3ffdfa3a9129c9660edaead54bb99a869
Alex O'Neill <alexoneill561@gmail.com>
2021-04-03 23:52:44 -0400

    more updates towards past presentfuture function

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # Day-Planner
 A virtual calendar that gives you time slots throughout the day to input what you are doing in those times.
 
+## Tchnologies used
+- HTML
+- CSS
+- Javascript
+- Bootstrap
+- JQuery
+
+# Links
 
--- script.js
+++ script.js
@@ -1,11 +1,11 @@
+var today = moment();
+
+
+
+
 $(document).ready(function () {
 
 
-
-    
-    
-    
-    
     $('.saveBtn').on('click', function () {
         console.log("click button")
         var textValue = $(this).siblings('.description').val();
@@ -13,49 +13,42 @@ $(document).ready(function () {
         console.log(textValue);
         console.log(textTime);
         localStorage.setItem(textValue, textTime);
+
+        
+    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+    
+    $(".time-block").each(function () {
+        var id = $(this).attr("id");
+        var schedule = localStorage.getItem(id);
+       }
         
 
 
 
-    })
+    });
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        
+    function pastPresentFuture() {
+        hour = time.hours();
+        $(".time-block").each(function () {
+            var thisHour = parseInt($(this).attr("id"));
+            
+            if (thisHour > hour) {
+                $(this).addClass("future")
+            }
+            else if (thisHour === hour) {
+                $(this).addClass("present");
+            }
+            else {
+                $(this).addClass("past");
+            }
+        })
+    }
+    
+    pastPresentFuture();
 
 
 
