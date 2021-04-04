d195c3719b0549eff66a091c19749cd74ed5c055
Alex O'Neill <alexoneill561@gmail.com>
2021-04-03 23:17:16 -0400

    more updates to js functions

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 $(document).ready(function () {
 
+
+
+    
+    
+    
+    
     $('.saveBtn').on('click', function () {
         console.log("click button")
+        var textValue = $(this).siblings('.description').val();
+        var textTime = $(this).parent().attr('id');
+        console.log(textValue);
+        console.log(textTime);
+        localStorage.setItem(textValue, textTime);
+        
 
 
 
