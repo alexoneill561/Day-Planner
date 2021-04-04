9669322fe07f0e4c5af86e1e09b1844c825fa299
Alex O'Neill <alexoneill561@gmail.com>
2021-04-03 21:32:42 -0400

    initial updates towards html

---
+++ script.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+
+    $('.saveBtn').on('click', function () {
+        console.log("click button")
+
+
+
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
